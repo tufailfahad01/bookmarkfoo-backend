f6c93cee83801babceeab013e355104a68fd206c
Fahad Tufail <tufailfahad01@gmail.com>
2024-05-03 18:14:50 +0500

    empty commit

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:true});
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe)
-  const PORT=Number(process.env.PORT) || 3000;
-  console.log('port=',PORT)
+  const PORT = Number(process.env.PORT) || 3000;
+  console.log('port=', PORT)
   await app.listen(PORT);
 }
+
 bootstrap();
