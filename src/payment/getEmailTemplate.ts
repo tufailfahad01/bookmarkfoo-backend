2962e0d342b7576296d2ecc935ba71e156fed42f
Fahad Tufail <tufailfahad01@gmail.com>
2024-05-27 11:01:09 +0500

    Update email content

--- src/payment/getEmailTemplate.ts
+++ src/payment/getEmailTemplate.ts
@@ -1,12 +1,12 @@
 export const emailTemplate = (name: string, categories: number, price: number) => {
   return `
     Hi ${name},
-    Thanks for using Bookmark Foo! Your links are attached - when you open your links bookmark them to your favorite browser for easy access later.
+    Thanks for using BookmarkFu! Your links are attached - when you open your links bookmark them to your favorite browser for easy access later.
 
     You can download your links here 
+
+    Here’s your special offer ${categories} categories for only ${price} to: We are constantly adding new exciting categories - Check back soon!'
     
-    Here’s your special offer ${categories} categories for only $${price}
-    Enjoy! 
     Bookmark A team
   `
 }
