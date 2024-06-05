31212e80a61661d6f5fb45c709cc4940b45757dc
Fahad Tufail <tufailfahad01@gmail.com>
2024-06-05 11:28:38 +0500

    update email template

--- src/payment/getEmailTemplate.ts
+++ src/payment/getEmailTemplate.ts
@@ -1,12 +1,18 @@
-export const emailTemplate = (name: string, categories: number, price: number) => {
+export const emailTemplate = (
+  name: string,
+  categories: number,
+  price: number,
+) => {
   return `
     Hi ${name},
-    Thanks for using BookmarkFu! Your links are attached - when you open your links bookmark them to your favorite browser for easy access later.
-
-    You can download your links here 
-
-    Here’s your special offer ${categories} categories for only ${price} to: We are constantly adding new exciting categories - Check back soon!'
     
-    Bookmark A team
-  `
-}
+    Thanks for using BookmarkFu!
+    Your categories and links are attached - when you open your links bookmark them to your favorite browser for easy 
+    access later.
+
+    We are constantly adding new exciting categories - Check back soon!
+
+    Enjoy!
+    Bookmark Fu A team
+  `;
+};
