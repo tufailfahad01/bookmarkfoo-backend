71e50037cdbadd4cad91d0e83429750c9c31c4a7
Fahad Tufail <tufailfahad01@gmail.com>
2024-06-24 11:33:14 +0500

    JWT token will not expire

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,31 +1,30 @@
-import { Module, forwardRef } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { JwtModule } from "@nestjs/jwt";
-import { PassportModule } from "@nestjs/passport";
-import { JwtStrategy } from "./jwt.strategy";
-import { AuthService } from "./auth.service";
-import { AuthController } from "./auth.controller";
-import { MongooseModule } from "@nestjs/mongoose";
-import { UserSchema } from "src/schemas/user.schema";
-import { UsersModule } from "src/users/users.module";
-
+import { Module, forwardRef } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from 'src/schemas/user.schema';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-    imports:[
-       forwardRef(()=>UsersModule),
-        PassportModule,
+  imports: [
+    forwardRef(() => UsersModule),
+    PassportModule,
     JwtModule.registerAsync({
-        imports:[ConfigModule],
-        useFactory: async (configService:ConfigService)=>({
-            secret: configService.get<string>('SECRET'),
-            signOptions:{expiresIn: configService.get<string>('ExpireIn')}
-        }),
-        inject:[ConfigService]
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('SECRET'),
+        // signOptions: { expiresIn: configService.get<string>('ExpireIn') }, // unlimited expirey date
+      }),
+      inject: [ConfigService],
     }),
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
-    ],
-    controllers:[AuthController],
-    providers:[AuthService,JwtStrategy],
-    exports:[JwtStrategy,PassportModule,JwtModule]
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule, JwtModule],
 })
-export class AuthModule{}
+export class AuthModule {}
