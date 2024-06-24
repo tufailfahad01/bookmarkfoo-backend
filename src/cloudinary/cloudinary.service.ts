f4f145b44e9f9c8165efda559ee1b895d84bf579
Fahad Tufail <tufailfahad01@gmail.com>
2024-06-24 16:34:26 +0500

    allow svgs

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -5,24 +5,41 @@ const streamifier = require('streamifier');
 
 @Injectable()
 export class CloudinaryService {
-  uploadImage(file: Express.Multer.File): Promise<UploadApiErrorResponse | UploadApiResponse> {
+  uploadImage(
+    file: Express.Multer.File,
+  ): Promise<UploadApiErrorResponse | UploadApiResponse> {
     try {
-      if (!file) throw new BadRequestException("Invalide File");
-      const allowedExtensions = ['.png', '.jpg', '.gif', '.jpeg', '.pdf', '.xls', '.xlsx'];
-      const fileExtension = '.' + file.originalname.split('.').pop().toLowerCase();
+      if (!file) throw new BadRequestException('Invalide File');
+      const allowedExtensions = [
+        '.png',
+        '.jpg',
+        '.gif',
+        '.jpeg',
+        '.pdf',
+        '.xls',
+        '.xlsx',
+        '.svg',
+      ];
+      const fileExtension =
+        '.' + file.originalname.split('.').pop().toLowerCase();
 
       if (!allowedExtensions.includes(fileExtension)) {
-        throw new BadRequestException("Only images are allowed");
+        throw new BadRequestException('Only images are allowed');
       }
-      return new Promise<UploadApiErrorResponse | UploadApiResponse>((resolve, reject) => {
-        const uploadStream = cloudinary.uploader.upload_stream({ folder: 'bookmark-foo' }, (error, result) => {
-          if (error) return reject(error);
-          resolve(result);
-        });
-        streamifier.createReadStream(file.buffer).pipe(uploadStream);
-      });
+      return new Promise<UploadApiErrorResponse | UploadApiResponse>(
+        (resolve, reject) => {
+          const uploadStream = cloudinary.uploader.upload_stream(
+            { folder: 'bookmark-foo' },
+            (error, result) => {
+              if (error) return reject(error);
+              resolve(result);
+            },
+          );
+          streamifier.createReadStream(file.buffer).pipe(uploadStream);
+        },
+      );
     } catch (error) {
-      throw new BadRequestException(error.message)
+      throw new BadRequestException(error.message);
     }
   }
 }
